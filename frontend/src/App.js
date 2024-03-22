25ebf0c73bdf66c6920496a99bb2af53a71902ad
ahmed thousif <ahmedthousif571@gmail.com>
2024-03-22 19:28:38 +0530

    sign login design done

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header'
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
